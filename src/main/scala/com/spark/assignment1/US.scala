a8e415492bce99b64f5bd231540fe0064ce966f5
hariinfo <hariinfo@gmail.com>
2020-04-12 18:10:22 -0500

    removed assignment files

--- src/main/scala/com/spark/assignment1/US.scala
+++
@@ -1,9 +0,0 @@
-package com.spark.assignment1
-
-case class US(
-               Province_State: String,
-               Country_Region: String,
-               Lat: String,
-               Long: String,
-
-                  )

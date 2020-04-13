d0e30cf7c15a57e44d002511fcacc16155e38bfd
hariinfo <hariinfo@gmail.com>
2020-04-12 19:29:35 -0500

    cleared data

--- src/main/scala/com/spark/assignment1/Airline.scala
+++ src/main/scala/com/spark/assignment1/Airline.scala
@@ -1,5 +1,21 @@
 package com.spark.assignment1
 
+import java.util.Date
+
 case class Airline(
-                    Reporting_Airline: String
+                    Year: Long,
+                    Month: Long,
+                    Reporting_Airline: String,
+                    FlightDate: String,
+                    OriginState: String,
+                    OriginStateName: String,
+                    DestState: String,
+                    DestStateName: String,
+                    ArrDel15: Double,
+                    DepDel15: Double,
+                    CarrierDelay: Int,
+                    WeatherDelay: Int,
+                    NASDelay: Int,
+                    SecurityDelay: Int,
+                    LateAircraftDelay: Int
                   )
